7adcf344933d0e0d7eeac6ef205bac177f89d52e
raissaraden <sardnz98@gmail.com>
2025-11-27 07:37:04 +0700

    rapihkan

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.awt.geom.*;
-import java.util.*;
+
 
 public class Main {
     public static void main(String[] args) {
